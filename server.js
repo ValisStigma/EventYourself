31b78204e313bc13a1f69ff12e2ed77071f77b46
ValisStigma <rafael_krucker@hotmail.com>
2015-11-28 13:01:32 +0100

    Added auth

--- server.js
+++ server.js
@@ -4,9 +4,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var session = require('express-session');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var uuid = require('uuid');
+
 var events = require('./server_routes/events');
 var feedback = require('./server_routes/feedback');
-
+var login = require('./server_routes/login');
 
 var allowCrossDomain = function(request, response, next) {
     response.header('Access-Control-Allow-Origin', '*');
@@ -29,9 +36,13 @@ app.use('/', express.static(__dirname + '/public/webapp'));
 app.use('/tests', express.static(__dirname + '/webapp/tests'));
 app.use('/source', express.static(__dirname + '/webapp/source'));
 
+app.use(session({
+    secret: uuid.v1()
+}));
 //Route config
 app.use('/api/events', events);
 app.use('/api/feedback', feedback);
+app.use('/api/auth', login);
 
 app.on("connection", function (socket) {
 	socket.setNoDelay(true);
