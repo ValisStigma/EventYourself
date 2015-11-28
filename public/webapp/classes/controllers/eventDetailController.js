db14d65d716e5c3102b08b58eaed65e8f3be685b
ValisStigma <rafael_krucker@hotmail.com>
2015-11-28 20:14:50 +0100

    mega krass de bug fix

--- public/webapp/classes/controllers/eventDetailController.js
+++ public/webapp/classes/controllers/eventDetailController.js
@@ -6,38 +6,12 @@ define([], function( ){
 		EventsService.getEventById($routeParams.id).then(
 			function ( event ) {
 				$scope.event = event;
-				EventsService.getComments($routeParams.id).then(
-					function(comments) {
-						if(comments && comments.length > 0) {
-							$scope.event.comments = comments;
-						}
-
-					},
-						function(error) {
-							$scope.isError = true;
-							$scope.errorMsg = error;}
-				);
 			},
 			function ( error ) { $scope.isError = true; $scope.errorMsg = error; }
 		);
 
 		$scope.getImagePath = ConfigService.basePathCreator('images/events');
-		$scope.comment = { author: '', msg: '' };
-		$scope.submit = function ( ) {
-			var author = $scope.comment.author,
-					msg = $scope.comment.msg;
 
-			if(msg.length == 0) return;
-
-			EventsService.createComment($routeParams.id, {author: author, text: msg}).then(
-				function ( comment ) {
-					$scope.comment = {author: '', msg: ''};
-					$scope.commentSent = true;
-					$scope.event.comments.push(comment);
-				},
-				function ( error ) { console.log(error); }
-			);
-		};
 
 		$scope.reset = function () {
 			$route.reload();
